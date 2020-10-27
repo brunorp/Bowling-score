7d1248f58385c9a7acbea4a6a20f7610b764ce04
Bruno Rossetto Pereira <bruno.rossetto@ibm.com>
2020-10-27 15:37:47 -0300

    added some tests and changed functions: total score and play

--- BowlingGame.Tests/UnitTest1.cs
+++ BowlingGame.Tests/UnitTest1.cs
@@ -6,15 +6,93 @@ namespace BowlingGame.Tests
     public class ScoreTests
     {
         [Fact]
-        public void Test1()
+        public void AllTests()
         {
-            var test1 = new BowlingGame();
-            for (int i = 0; i < 20; i++)
-            {
-                test1.Play(1);
-            }
+            AllZeroTest();
+            StrikeTest();
+            SpareTest();
+            CompleteGame();
+            PerfectGame();
+            StrikeAndSpareTest();
+        }
 
-            Assert.Equal(20, test1.TotalScore(test1.plays));
+        private void AllZeroTest()
+        {
+            var game = new BowlingGame();
+            PlaySequence(20, 0, game);
+            Assert.Equal(0, game.TotalScore());
+        }
+
+        private void PerfectGame()
+        {
+            var game = new BowlingGame();
+            PlaySequence(12, 10, game);
+            Assert.Equal(300, game.TotalScore());
+        }
+
+        private void StrikeTest()
+        {
+            var game = new BowlingGame();
+            game.Play(10);
+            game.Play(5);
+            game.Play(3);
+            PlaySequence(17, 0, game);
+            Assert.Equal(26, game.TotalScore());
+        }
+
+        private void SpareTest()
+        {
+            var game = new BowlingGame();
+            game.Play(6);
+            game.Play(4);
+            game.Play(3);
+            PlaySequence(17, 0, game);
+            Assert.Equal(16, game.TotalScore());
+        }
+
+        private void StrikeAndSpareTest()
+        {
+            var game = new BowlingGame();
+            game.Play(10);
+            game.Play(4);
+            game.Play(6); 
+            game.Play(3);
+            game.Play(0);
+            PlaySequence(15, 0, game);
+            Assert.Equal(36, game.TotalScore());
+        }
+
+        private void CompleteGame()
+        {
+            var game = new BowlingGame();
+            game.Play(6);
+            game.Play(4);
+            game.Play(3);
+            game.Play(1);
+            game.Play(10);
+            game.Play(7);
+            game.Play(1);
+            game.Play(5);
+            game.Play(4);
+            game.Play(10);
+            game.Play(1);
+            game.Play(2);
+            game.Play(4);
+            game.Play(6);
+            game.Play(5);
+            game.Play(2);
+            game.Play(0);
+            game.Play(7);
+            Assert.Equal(97, game.TotalScore());
+        }
+
+        private void PlaySequence(int numberOfPlays, int bowlingPins, BowlingGame game)
+        {
+            for(int i = 0; i < numberOfPlays; i++)
+            {
+                game.Play(bowlingPins);
+            }
         }
     }
 }
+
--- BowlingGame/BowlingGame.cs
+++ BowlingGame/BowlingGame.cs
@@ -5,30 +5,49 @@ namespace BowlingGame
 {
     public class BowlingGame
     {
-        public List<int> plays = new List<int>();
+        private List<int> plays = new List<int>();
 
-        private int currentIndex = 0;
-        private int totalScore = 0;
-
-        public BowlingGame()
-        {
-        }
+        private int currentPlay = 0;
 
         public void Play(int bowlingPins)
-        {            
-            plays.Insert(currentIndex, bowlingPins);
-            currentIndex += 1;
-            totalScore += bowlingPins;
+        {   
+            if (bowlingPins == 10)
+            {
+                plays.Insert(currentPlay, bowlingPins);
+                plays.Insert(currentPlay + 1, 0);
+                currentPlay += 2;
+            }
+            else
+            {
+                plays.Insert(currentPlay, bowlingPins);
+                currentPlay += 1;
+            }            
         }
 
-        public int TotalScore(List<int> plays)
+        public int TotalScore()
         {
             if (plays.Count == 0)
                 return 0;
 
             int total = 0;
-            foreach(int play in plays)
-                total += play;
+            int index = 0;
+
+            for(int i = 0; i < 10; i++)
+            {
+                if (plays[index] == 10)
+                {
+                    if(plays[index+2] == 10)
+                        total += (10 + plays[index + 2] + plays[index + 4]);
+                    else
+                        total += (10 + plays[index + 2] + plays[index + 3]);
+                }
+                else if (plays[index] + plays[index + 1] == 10)
+                    total += (10 + plays[index + 2]);
+                else
+                    total += plays[index] + plays[index + 1];
+                
+                index += 2;
+            }
             
             return total;
         }
