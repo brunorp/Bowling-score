ee0581f160bad2e9dde89f2c7709b9dbd67f90b7
Bruno Rossetto Pereira <bruno.rossetto@ibm.com>
2020-10-28 11:30:11 -0300

    feature: verify score type

--- BowlingGame/BowlingGame.cs
+++ BowlingGame/BowlingGame.cs
@@ -6,9 +6,10 @@ namespace BowlingGame
     public class BowlingGame
     {
         private List<int> plays = new List<int>();
-
+       
         private int currentPlay = 0;
 
+        //Make the moves by entering the number of bowling pins that have been dropped in a list
         public void Play(int bowlingPins)
         {   
             if (bowlingPins == 10)
@@ -24,32 +25,45 @@ namespace BowlingGame
             }            
         }
 
+        //Calculates the total score, check for strikes and spares and deal with it
         public int TotalScore()
         {
             if (plays.Count == 0)
                 return 0;
 
-            int total = 0;
-            int index = 0;
+            int totalOfPoints = 0;
+            int index = 0; //index of moves
 
             for(int i = 0; i < 10; i++)
             {
-                if (plays[index] == 10)
-                {
-                    if(plays[index+2] == 10)
-                        total += (10 + plays[index + 2] + plays[index + 4]);
-                    else
-                        total += (10 + plays[index + 2] + plays[index + 3]);
-                }
-                else if (plays[index] + plays[index + 1] == 10)
-                    total += (10 + plays[index + 2]);
+                if(VerifyScoreType(index) == "multiple strike")  
+                    totalOfPoints += (10 + plays[index + 2] + plays[index + 4]);
+                else if(VerifyScoreType(index) == "strike")
+                    totalOfPoints += (10 + plays[index + 2] + plays[index + 3]);
+                else if (VerifyScoreType(index) == "spare")
+                    totalOfPoints += (10 + plays[index + 2]);
                 else
-                    total += plays[index] + plays[index + 1];
+                    totalOfPoints += plays[index] + plays[index + 1];
                 
                 index += 2;
             }
             
-            return total;
+            return totalOfPoints;
+        }
+
+        public string VerifyScoreType(int index) 
+        {
+            if (plays[index] == 10)
+            {
+                if (plays[index + 2] == 10)
+                    return "multiple strike";
+                else
+                    return "strike";
+            }
+            else if (plays[index] + plays[index + 1] == 10)
+                return "spare";
+            else
+                return "normal";
         }
     }
 }
