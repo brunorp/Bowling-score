1538856165a378a61d05ce41d735c8bfbe95d20c
Bruno Rossetto Pereira <bruno.rossetto@ibm.com>
2020-10-29 13:57:27 -0300

    features: spare, strike and PlayFrame

--- BowlingGame.Tests/UnitTest1.cs
+++ BowlingGame.Tests/UnitTest1.cs
@@ -7,49 +7,32 @@ namespace BowlingGame.Tests
     public class ScoreTests
     {
         [Fact]
-        public void AllTests()
-        {
-            AllZeroTest();
-            StrikeTest();
-            SpareTest();
-            CompleteGame();
-            PerfectGame();
-            StrikeAndSpareTest();
-            testtt();
-        }
-
-        private void testtt()
+        private void CompleteGameFramesScoreTest()
         {
             var game = new BowlingGame();
-            game.Play(6);
-            game.Play(4);
-            game.Play(3);
-            game.Play(1);
-            game.Play(10);
-            game.Play(10);
-            game.Play(1);
-            game.Play(5);
-            game.Play(10);
-            game.Play(1);
-            game.Play(2);
-            game.Play(4);
-            game.Play(6);
-            game.Play(6);
-            game.Play(4);
-            game.Play(6);
-            game.Play(1);
+            Spare(game);
+            PlayFrame(3, 1, game);
+            Strike(game);
+            Strike(game);
+            PlayFrame(5, 1, game);
+            Strike(game);
+            PlayFrame(2, 1, game);
+            Spare(game);
+            Spare(game);
+            PlayFrame(6, 1, game);
             Assert.Equal(13, game.TotalScore(0));
             Assert.Equal(17, game.TotalScore(1));
-            Assert.Equal(38, game.TotalScore(2));
-            Assert.Equal(54, game.TotalScore(3));
-            Assert.Equal(60, game.TotalScore(4));
-            Assert.Equal(73, game.TotalScore(5));
-            Assert.Equal(76, game.TotalScore(6));
-            Assert.Equal(92, game.TotalScore(7));
-            Assert.Equal(108, game.TotalScore(8));
-            Assert.Equal(115, game.TotalScore(9));
+            Assert.Equal(42, game.TotalScore(2));
+            Assert.Equal(58, game.TotalScore(3));
+            Assert.Equal(64, game.TotalScore(4));
+            Assert.Equal(77, game.TotalScore(5));
+            Assert.Equal(80, game.TotalScore(6));
+            Assert.Equal(96, game.TotalScore(7));
+            Assert.Equal(112, game.TotalScore(8));
+            Assert.Equal(119, game.TotalScore(9));
         }
 
+        [Fact]
         private void AllZeroTest()
         {
             var game = new BowlingGame();
@@ -57,6 +40,15 @@ namespace BowlingGame.Tests
             Assert.Equal(0, game.TotalScore());
         }
 
+        [Fact]
+        private void AllOneTest()
+        {
+            var game = new BowlingGame();
+            PlaySequence(20, 1, game);
+            Assert.Equal(20, game.TotalScore());
+        }
+
+        [Fact]
         private void PerfectGame()
         {
             var game = new BowlingGame();
@@ -64,68 +56,76 @@ namespace BowlingGame.Tests
             Assert.Equal(300, game.TotalScore());
         }
 
+        [Fact]
         private void StrikeTest()
         {
             var game = new BowlingGame();
-            game.Play(10);
-            game.Play(5);
-            game.Play(3);
+            Strike(game);
+            PlayFrame(5, 3, game);
             PlaySequence(17, 0, game);
             Assert.Equal(26, game.TotalScore());
         }
 
+        [Fact]
         private void SpareTest()
         {
             var game = new BowlingGame();
-            game.Play(6);
-            game.Play(4);
+            Spare(game);
             game.Play(3);
             PlaySequence(17, 0, game);
             Assert.Equal(16, game.TotalScore());
         }
 
+        [Fact]
         private void StrikeAndSpareTest()
         {
             var game = new BowlingGame();
-            game.Play(10);
-            game.Play(4);
-            game.Play(6); 
-            game.Play(3);
-            game.Play(0);
-            PlaySequence(15, 0, game);
-            Assert.Equal(36, game.TotalScore());
+            Strike(game);
+            Spare(game);
+            PlaySequence(17, 0, game);
+            Assert.Equal(30, game.TotalScore());
         }
 
+        [Fact]
         private void CompleteGame()
         {
             var game = new BowlingGame();
-            game.Play(6);
-            game.Play(4);
-            game.Play(3);
-            game.Play(1);
-            game.Play(10);
-            game.Play(7);
-            game.Play(1);
-            game.Play(5);
-            game.Play(4);
-            game.Play(10);
-            game.Play(1);
-            game.Play(2);
-            game.Play(4);
-            game.Play(6);
-            game.Play(5);
-            game.Play(2);
-            game.Play(0);
-            game.Play(7);
+            Spare(game);
+            PlayFrame(3, 1, game);
+            Strike(game);
+            PlayFrame(7, 1, game);
+            PlayFrame(5, 4, game);
+            Strike(game);
+            PlayFrame(2, 1, game);
+            Spare(game);
+            PlayFrame(5, 2, game);
+            PlayFrame(0, 7, game);
             Assert.Equal(97, game.TotalScore());
         }
 
         private void PlaySequence(int numberOfPlays, int bowlingPins, BowlingGame game)
         {
-            for(int i = 0; i < numberOfPlays; i++)
+            for (int i = 0; i < numberOfPlays; i++)
             {
                 game.Play(bowlingPins);
             }
+        }
+
+        private void PlayFrame(int bowlingPins, int pins, BowlingGame game)
+        {
+            game.Play(bowlingPins);
+            game.Play(pins);
+        }
+
+        private void Strike(BowlingGame game)
+        {
+            game.Play(10);
+        }
+
+        private void Spare(BowlingGame game)
+        {
+            game.Play(6);
+            game.Play(4);
         }
     }
 }
